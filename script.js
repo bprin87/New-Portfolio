c2f979eb456e17cc6c01b14ce1d5f6c3ab14ae40
Ben <123553196+bprin87@users.noreply.github.com>
2025-03-21 13:21:32 +0000

    added enquiry modal

--- script.js
+++ script.js
@@ -3,8 +3,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     let tl = gsap.timeline();
 
+        
+    tl.from(".site-logo", {
+        x: -100,
+        rotation: -360,
+        opacity: 0,
+        duration: 0.8,
+        ease: "power2.out"
+    });
+    
+
     // Step 1: Animate Navigation Items First
-    tl.from(".site-logo, .nav-menu li, .website-enquiry button", {
+    tl.from(".nav-menu li, .website-enquiry button", {
         x: -100,
         opacity: 0,
         duration: 0.6,
@@ -19,3 +29,47 @@ document.addEventListener("DOMContentLoaded", function () {
       .from(".hero p", { opacity: 0, y: -30, duration: 0.8, ease: "power2.out" }, "-=0.4")
       .from(".hero button", { opacity: 0, y: -30, duration: 0.8, ease: "power2.out" }, "-=0.4");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    gsap.to("textPath", {
+        duration: 6,
+        repeat: -1,
+        ease: "linear",
+        attr: { startOffset: "100%" }
+    });
+});
+
+// Open and close enquiry modal
+document.addEventListener("DOMContentLoaded", function () {
+
+    const modal = document.querySelector(".enquiry-modal");
+    const openModalButtons = document.querySelectorAll(".open-modal");
+    const closeModalButton = document.querySelector(".close-enquiry-modal");
+    const modalBackground = document.querySelector(".modal-background");
+    const body = document.body;
+
+    // Ensure modal is hidden when the page loads
+    modalBackground.style.display = "none";
+
+        // Open modal on any button click
+        openModalButtons.forEach(button => {
+            button.addEventListener("click", function (e) {
+                e.preventDefault();
+                modalBackground.style.display = "flex";
+
+                // Disable Scrolling
+                modal.style.display = "block";
+                body.style.overflow = "hidden";
+                modal.scrollTop = 0;
+            });
+        });
+    
+            // Close modal
+    closeModalButton.addEventListener("click", function () {
+        modalBackground.style.display = "none";
+        modal.style.display = "none";
+        body.style.overflow = "auto";
+    });
+    
+});
+
